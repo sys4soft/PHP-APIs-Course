490448ff6bf9a5eb43fbe8bdef1b702da78f5333
João Ribeiro <business@sys4soft.com>
2021-11-06 21:02:07 +0000

    Add files via upload

---
+++ api/inc/api_logic.php
@@ -0,0 +1,58 @@
+<?php
+
+class api_logic
+{
+
+    private $endpoint;
+    private $params;
+
+    // --------------------------------------------------
+    public function __construct($endpoint, $params = null)
+    {
+        // define the object/class properties
+        $this->endpoint = $endpoint;
+        $this->params = $params;
+    }
+
+    // --------------------------------------------------
+    public function endpoint_exists()
+    {
+        // check if the endpoint is a valid class method
+        return method_exists($this, $this->endpoint);
+    }
+
+
+    // --------------------------------------------------
+    // ENDPOINTS
+    // --------------------------------------------------
+    public function status()
+    {
+        return [
+            'status' => 'SUCCESS',
+            'message' => 'API is running ok!',
+            'results' => null
+        ];
+    }
+
+    // --------------------------------------------------
+    public function get_all_clients()
+    {
+
+        // query to the database
+
+        return [
+            'status' => 'SUCCESS',
+            'message' => '',
+            'results' => [
+                'joao','ana','pedro','antónio'
+            ]
+        ];
+    }
+
+    public function get_all_products()
+    {
+
+    }
+
+
+}
