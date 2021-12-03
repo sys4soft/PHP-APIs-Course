d82c96cbe188a06cf4cb5dac214638a53d4e3f85
João Ribeiro <business@sys4soft.com>
2021-12-03 22:59:26 +0000

    Add files via upload

---
+++ app/inc/nav.php
@@ -0,0 +1,10 @@
+<header class="container-fluid text-center p-2 bg-dark text-white">
+    <div class="row">
+        <div class="col">
+            <h1>App Consumidora</h1>
+            <a class="btn btn-secondary btn-sm mx-2" href="index.php">HOME</a>
+            <a class="btn btn-secondary btn-sm mx-2" href="clientes.php">Clientes</a>
+            <a class="btn btn-secondary btn-sm mx-2" href="produtos.php">Produtos</a>
+        </div>
+    </div>
+</header>
