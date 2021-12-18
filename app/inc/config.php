618bb0f01e97dca7d294bdd1df7fd98f37b85c94
João Ribeiro <business@sys4soft.com>
2021-12-18 23:27:18 +0000

    Add files via upload

---
+++ app/inc/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('AULA', 'aula_08');
+
+define('API_BASE_ENDPOINT', 'http://localhost/http_simple_authentication/' . AULA . '/api/');
+define('API_USER', 'Carlos');
+define('API_PASS', '222');
