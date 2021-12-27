63d2a4d7d640c9b99f7fb01d65ed57455603d562
João Ribeiro <business@sys4soft.com>
2021-12-27 12:40:35 +0000

    Add files via upload

---
+++ admin/inc/html_footer.php
@@ -0,0 +1,3 @@
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ admin/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    require_once('inc/config.php');
+    require_once('inc/database.php');
+
+
+    require_once('inc/html_header.php');
+
+
+    echo '<h3>Olá Mundo!</h3>';
+
+
+
+    require_once('inc/html_footer.php');
